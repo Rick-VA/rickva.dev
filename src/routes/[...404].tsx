79a0ae28ee6deb7ed7e27b93c4a8c28cc2cf40e3
Rick van Assen <rick@brainhive.nl>
2024-11-15 14:45:58 +0100

    feat: set http code 418

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,7 +1,10 @@
 import { A } from "@solidjs/router";
+import { HttpStatusCode } from "@solidjs/start";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function NotFound() {
+  <HttpStatusCode code={418} />
+
   return (
     <main class="flex flex-col items-center justify-center min-h-screen mx-auto pb-4">
       <h1 style={{ "font-size": "3rem" }}>418 - I'm a teapot</h1>
